4ea6ef134f5ea0da2d95e56fa2f15eba3e491c95
shivanidoshi26 <shivanidoshi26@gmail.com>
2021-04-15 20:06:42 -0400

    fixed face detection

--- Lab 5/face-detection/face-detection.py
+++ Lab 5/face-detection/face-detection.py
@@ -44,20 +44,20 @@ else:
       print("Using default image.")
 
 i = 0
+
 while(True):
    if webCam:
       ret, img = cap.read()
 
    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
    copy = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
 
    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
    for (x,y,w,h) in faces:
        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-       cv2.putText(img, "Wanna take a photo?",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
+       cv2.putText(img, "Want to take a photo?",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,),2,cv2.LINE_AA)
        if buttonR.is_button_pressed():
-           cv2.imwrite('pic0' + str(i) + '.jpg',copy)
+           cv2.imwrite('pic00' + str(i) + '.jpg',copy)
            i += 1
            time.sleep(0.2)
 
@@ -69,6 +69,5 @@ while(True):
    else:
       break
 
-#cv2.imwrite('faces_detected.jpg',img)
 cv2.destroyAllWindows()
 
