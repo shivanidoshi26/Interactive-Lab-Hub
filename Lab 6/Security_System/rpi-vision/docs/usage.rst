d93abeda03d1a9885b6f395767eacd12dadb3b86
shivanidoshi26 <shivanidoshi26@gmail.com>
2021-04-30 15:44:46 -0400

    deleted old idea

--- Lab 6/Security_System/rpi-vision/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Computer Vision on Raspberry Pi in a project::
-
-    import rpi_vision
--- Lab 6/Security_System/rpi-vision/rpi_vision/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Computer Vision on Raspberry Pi."""
-
-__author__ = """Leigh Johnson"""
-__email__ = 'leigh@data-literate.com'
-__version__ = '0.1.0'
